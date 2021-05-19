94a06b835db7f85648c4cf2ea5bd617668ebdde8
charles.davernas <charles.davernas@neuroglia.io>
2021-05-19 18:16:18 +0200

    - Add DelayStateValidator and InjectStateValidator
    Signed-off-by: charles.davernas <charles.davernas@neuroglia.io>

---
+++ src/ServerlessWorkflow.Sdk/Services/Validation/DelayStateValidator.cs
@@ -0,0 +1,24 @@
+﻿using ServerlessWorkflow.Sdk.Models;
+
+namespace ServerlessWorkflow.Sdk.Services.Validation
+{
+    /// <summary>
+    /// Represents a service used to validate <see cref="DelayStateDefinition"/>s
+    /// </summary>
+    public class DelayStateValidator
+        : StateDefinitionValidator<DelayStateDefinition>
+    {
+
+        /// <summary>
+        /// Initializes a new <see cref="DelayStateValidator"/>
+        /// </summary>
+        /// <param name="workflow">The <see cref="WorkflowDefinition"/> to validate</param>
+        public DelayStateValidator(WorkflowDefinition workflow)
+            : base(workflow)
+        {
+            
+        }
+
+    }
+
+}
