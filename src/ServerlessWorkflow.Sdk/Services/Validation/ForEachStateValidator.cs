960b20525e5e5c60ceb303974a913121a95f9f70
charles.davernas <charles.davernas@neuroglia.io>
2021-05-27 10:34:57 +0200

    Add the SubflowReference property to the ActionDefinition
    Signed-off-by: charles.davernas <charles.davernas@neuroglia.io>

---
+++ src/ServerlessWorkflow.Sdk/Services/Validation/ForEachStateValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using ServerlessWorkflow.Sdk.Models;
+using System.Linq;
+
+namespace ServerlessWorkflow.Sdk.Services.Validation
+{
+    /// <summary>
+    /// Represents a service used to validate <see cref="ForEachStateDefinition"/>s
+    /// </summary>
+    public class ForEachStateValidator
+        : StateDefinitionValidator<ForEachStateDefinition>
+    {
+
+        /// <summary>
+        /// Initializes a new <see cref="ForEachStateValidator"/>
+        /// </summary>
+        /// <param name="workflow">The <see cref="WorkflowDefinition"/> to validate</param>
+        public ForEachStateValidator(WorkflowDefinition workflow)
+            : base(workflow)
+        {
+            this.RuleFor(s => s.Actions)
+                .NotEmpty()
+                .WithErrorCode($"{nameof(ForEachStateDefinition)}.{nameof(ForEachStateDefinition.Actions)}");
+            this.RuleForEach(s => s.Actions)
+                .SetValidator(new ActionDefinitionValidator(this.Workflow))
+                .When(s => s.Actions != null && s.Actions.Any())
+                .WithErrorCode($"{nameof(ForEachStateDefinition)}.{nameof(ForEachStateDefinition.Actions)}");
+        }
+
+    }
+
+}
