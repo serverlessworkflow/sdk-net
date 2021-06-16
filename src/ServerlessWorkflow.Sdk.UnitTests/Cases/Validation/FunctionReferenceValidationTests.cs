71ed920e2d5c84bd1424950a2903fea790982c8b
charles.davernas <charles.davernas@neuroglia.io>
2021-06-16 14:49:39 +0200

    - Added the 'SelectionSet' property to the FunctionReference definition - Added 'SelectionSet' validation in the FunctionReferenceValidator - Added missing copyright file headers - Added new FunctionReference validation tests

--- src/ServerlessWorkflow.Sdk.UnitTests/Cases/Validation/FunctionReferenceValidationTests.cs
+++ src/ServerlessWorkflow.Sdk.UnitTests/Cases/Validation/FunctionReferenceValidationTests.cs
@@ -14,7 +14,7 @@ namespace ServerlessWorkflow.Sdk.UnitTests.Cases.Validation
         {
             //arrange
             var workflow = WorkflowDefinition.Create("fake", "fake", "fake")
-                .StartsWith("fake", flow => flow.Callback())
+                .StartsWith("fake", flow => flow.Inject(new { }))
                 .End()
                 .Build();
             var functionRef = new FunctionReference();
@@ -27,7 +27,7 @@ namespace ServerlessWorkflow.Sdk.UnitTests.Cases.Validation
                 .NotBeNull();
             result.Errors.Should()
                 .NotBeNullOrEmpty()
-                .And.Contain(e => e.PropertyName == nameof(FunctionReference.Name));
+                .And.Contain(e => e.PropertyName == nameof(FunctionReference.RefName));
         }
 
         [Fact]
@@ -38,7 +38,7 @@ namespace ServerlessWorkflow.Sdk.UnitTests.Cases.Validation
                .StartsWith("fake", flow => flow.Callback())
                .End()
                .Build();
-            var functionRef = new FunctionReference() { Name = "fake" };
+            var functionRef = new FunctionReference() { RefName = "fake" };
 
             //act
             var result = new FunctionReferenceValidator(workflow).Validate(functionRef);
@@ -48,7 +48,59 @@ namespace ServerlessWorkflow.Sdk.UnitTests.Cases.Validation
                .NotBeNull();
             result.Errors.Should()
                 .NotBeNullOrEmpty()
-                .And.Contain(e => e.PropertyName == nameof(FunctionReference.Name));
+                .And.Contain(e => e.PropertyName == nameof(FunctionReference.RefName));
+        }
+
+        [Fact]
+        public void Validate_FunctionReference_GraphQL_SelectionSetEmpty_ShouldFail()
+        {
+            //arrange
+            var functionName = "fake";
+            var workflow = WorkflowDefinition.Create("fake", "fake", "fake")
+                .AddFunction(function => function
+                    .WithName(functionName)
+                    .OfType(FunctionType.GraphQL))
+                .StartsWith("fake", flow => flow
+                    .Execute(action => action.Invoke(functionName)))
+                .End()
+                .Build();
+            var functionRef = new FunctionReference() { RefName = functionName };
+
+            //act
+            var result = new FunctionReferenceValidator(workflow).Validate(functionRef);
+
+            //assert
+            result.Should()
+                .NotBeNull();
+            result.Errors.Should()
+                .NotBeNullOrEmpty()
+                .And.Contain(e => e.PropertyName == nameof(FunctionReference.SelectionSet));
+        }
+
+        [Fact]
+        public void Validate_FunctionReference_SelectionSetNotEmpty_ShouldFail()
+        {
+            //arrange
+            var functionName = "fake";
+            var workflow = WorkflowDefinition.Create("fake", "fake", "fake")
+                .AddFunction(function => function
+                    .WithName(functionName)
+                    .OfType(FunctionType.Rest))
+                .StartsWith("fake", flow => flow
+                    .Execute(action => action.Invoke(functionName)))
+                .End()
+                .Build();
+            var functionRef = new FunctionReference() { RefName = functionName, SelectionSet = "{ id, name }" };
+
+            //act
+            var result = new FunctionReferenceValidator(workflow).Validate(functionRef);
+
+            //assert
+            result.Should()
+                .NotBeNull();
+            result.Errors.Should()
+                .NotBeNullOrEmpty()
+                .And.Contain(e => e.PropertyName == nameof(FunctionReference.SelectionSet));
         }
 
     }
--- src/ServerlessWorkflow.Sdk/Services/FluentBuilders/Interfaces/IFunctionActionBuilder.cs
+++ src/ServerlessWorkflow.Sdk/Services/FluentBuilders/Interfaces/IFunctionActionBuilder.cs
@@ -19,6 +19,7 @@ using System.Collections.Generic;
 
 namespace ServerlessWorkflow.Sdk.Services.FluentBuilders
 {
+
     /// <summary>
     /// Defines the service used to build <see cref="ActionDefinition"/>s of type <see cref="ActionType.Function"/>
     /// </summary>
@@ -27,7 +28,15 @@ namespace ServerlessWorkflow.Sdk.Services.FluentBuilders
     {
 
         /// <summary>
-        /// Configures the <see cref="ActionDefinition"/> to use the specified argument when performing the function call
+        /// Configures the <see cref="FunctionDefinition"/> to use the specified <see href="https://spec.graphql.org/June2018/#sec-Selection-Sets">GraphQL selection set</see>
+        /// </summary>
+        /// <param name="selectionSet">The <see href="https://spec.graphql.org/June2018/#sec-Selection-Sets">GraphQL selection set</see> to use</param>
+        /// <remarks>Only supported for <see cref="FunctionDefinition"/>s of type <see cref="FunctionType.GraphQL"/></remarks>
+        /// <returns>The configured <see cref="IFunctionActionBuilder"/></returns>
+        IFunctionActionBuilder WithSelectionSet(string selectionSet);
+
+        /// <summary>
+        /// Configures the <see cref="FunctionDefinition"/> to use the specified argument when performing the function call
         /// </summary>
         /// <param name="name">The name of the argument to add</param>
         /// <param name="value">The value or workflow expression of the argument to add</param>
@@ -35,7 +44,7 @@ namespace ServerlessWorkflow.Sdk.Services.FluentBuilders
         IFunctionActionBuilder WithArgument(string name, string value);
 
         /// <summary>
-        /// Configures the <see cref="ActionDefinition"/> to use the specified argument when performing the function call
+        /// Configures the <see cref="FunctionDefinition"/> to use the specified argument when performing the function call
         /// </summary>
         /// <param name="args">An <see cref="IDictionary{TKey, TValue}"/> containing the name/value pairs of the arguments to use</param>
         /// <returns>The configured <see cref="IFunctionActionBuilder"/></returns>
