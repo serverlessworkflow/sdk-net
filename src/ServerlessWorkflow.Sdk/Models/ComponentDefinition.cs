d4f406984313c4c5a28ca015f64cc8e8f0f726bc
Charles d'Avernas <charles.davernas@neuroglia.io>
2024-10-01 09:17:17 +0200

    fix(Sdk): Removed the extensions property from the ComponentDefinition model
    Signed-off-by: Charles d'Avernas <charles.davernas@neuroglia.io>

--- src/ServerlessWorkflow.Sdk/Models/ComponentDefinition.cs
+++ src/ServerlessWorkflow.Sdk/Models/ComponentDefinition.cs
@@ -18,13 +18,8 @@ namespace ServerlessWorkflow.Sdk.Models;
 /// </summary>
 [DataContract]
 public abstract record ComponentDefinition
-    : IExtendable
 {
 
-    /// <summary>
-    /// Gets/sets a key/value mapping of the object's extension data, if any
-    /// </summary>
-    [DataMember(Name = "extensions", Order = 99), JsonPropertyName("extensions"), JsonPropertyOrder(99), YamlMember(Alias = "extensions", Order = 99)]
-    public virtual EquatableDictionary<string, object>? Extensions { get; set; }
+
 
 }
