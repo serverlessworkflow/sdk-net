ab150bec556c1a9338352cbb0e65c393e91f319f
Charles d'Avernas <charles.davernas@neuroglia.io>
2026-04-15 10:06:29 +0200

    feat: add documentation

--- src/ServerlessWorkflow.Sdk/Events/Tasks/TaskCompletedEvent.cs
+++ src/ServerlessWorkflow.Sdk/Events/Tasks/TaskCompletedEvent.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2024-Present The Synapse Authors
+﻿// Copyright © 2024-Present The Serverless Workflow Specification Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License"),
 // you may not use this file except in compliance with the License.
--- src/ServerlessWorkflow.Sdk/ServerlessWorkflowSpecificationDefaults.cs
+++ src/ServerlessWorkflow.Sdk/ServerlessWorkflowSpecificationDefaults.cs
@@ -1,4 +1,17 @@
-﻿namespace ServerlessWorkflow.Sdk;
+﻿// Copyright © 2024-Present The Serverless Workflow Specification Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace ServerlessWorkflow.Sdk;
 
 /// <summary>
 /// Exposes the default values for the Serverless Workflow Specification
@@ -29,7 +42,7 @@ public static class ServerlessWorkflowSpecificationDefaults
         {
 
             /// <summary>
-            /// Gets the type prefix for all workflow-related cloud events produced by Synapse
+            /// Gets the type prefix for all workflow-related cloud events produced by the Serverless Workflow Specification
             /// </summary>
             public const string TypePrefix = CloudEvents.TypePrefix + "workflow.";
 
@@ -168,7 +181,7 @@ public static class ServerlessWorkflowSpecificationDefaults
         {
 
             /// <summary>
-            /// Gets the type prefix for all task-related cloud events produced by Synapse
+            /// Gets the type prefix for all task-related cloud events produced by the Serverless Workflow Specification
             /// </summary>
             public const string TypePrefix = CloudEvents.TypePrefix + "task.";
 
